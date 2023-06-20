b745b2509786edccb155714affa74eca1b48aaff
Karthikeyan Shanmugam <karthikeyan@bangthetable.com>
2023-06-20 19:20:39 +0530

    feat: Add Passage login functionality

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,12 @@
+
+export default function Dashboard({isAuthorized, userID, todos}: any) {    
+    return(
+       <div>
+        <div>
+          <p>
+            You've logged in! 
+          </p>
+        </div>
+      </div>
+    )
+}
